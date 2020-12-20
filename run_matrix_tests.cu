c8d1cd234e1bb78f1842790edb391fb6f72c9ac0
tom ludwig <tom.k.ludwig@gmail.com>
2020-12-20 14:48:24 -0800

    Add project files.

---
+++ run_matrix_tests.cu
@@ -0,0 +1,9 @@
+/*goal: compare performance of a few sequential & parallel implementations of matrix multiplication*/
+
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <omp.h>
+#include <chrono>
