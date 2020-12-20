0751db134744f8923d5909fb51d7ed7b715ce0c0
tom ludwig <tom.k.ludwig@gmail.com>
2020-12-20 15:22:51 -0800

    implement function to initialize random matrix

--- run_matrix_tests.cu
+++ run_matrix_tests.cu
@@ -7,3 +7,28 @@
 #include <iostream>
 #include <omp.h>
 #include <chrono>
+#include <random>
+
+int random_uniform_initialize(double* m, int M, int N, double lower, double upper)
+{
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(lower, upper);
+
+	for (int i = 0; i < M * N; ++i)
+	{
+		*(m + i) = distribution(generator);
+	}
+	return 0;
+}
+
+int main()
+{
+	int M = 50;
+	int N = 60;
+	double mat1[3000];
+	std::cout << "initial, final values of mat1: " << mat1[0] << " " << mat1[1] << " " <<
+		mat1[2] << " ... " << mat1[2997] << " " << mat1[2998] << " " << mat1[2999] << std::endl;
+	int init_failed = random_uniform_initialize(mat1, M, N, 0.0, 0.5);
+	std::cout << "initial, final values of mat1: " << mat1[0] << " " << mat1[1] << " " <<
+		mat1[2] << " ... " << mat1[2997] << " " << mat1[2998] << " " << mat1[2999] << std::endl;
+}
