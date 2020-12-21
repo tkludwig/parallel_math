017dec1ce4e0db93beb4cae9777b52fd7292e807
tom ludwig <tom.k.ludwig@gmail.com>
2020-12-20 16:06:32 -0800

    implement sequential naive matrix multiply

--- run_matrix_tests.cu
+++ run_matrix_tests.cu
@@ -21,6 +21,22 @@ int random_uniform_initialize(double* m, int M, int N, double lower, double uppe
 	return 0;
 }
 
+int sequential_naive_multiply(double* A, double* B, double* C, int M, int K, int N)
+{
+	for (int i = 0; i < M * N; ++i)
+	{
+		double val = 0.;
+		int row = i % M;
+		int col = i / M;
+		for (int j = 0; j < K; ++j)
+		{
+			val += *(A + j * M + row) * *(B + col * K + j);
+		}
+		*(C + i) = val;
+	}
+	return 0;
+}
+
 int main()
 {
 	int M = 50;
@@ -31,4 +47,11 @@ int main()
 	int init_failed = random_uniform_initialize(mat1, M, N, 0.0, 0.5);
 	std::cout << "initial, final values of mat1: " << mat1[0] << " " << mat1[1] << " " <<
 		mat1[2] << " ... " << mat1[2997] << " " << mat1[2998] << " " << mat1[2999] << std::endl;
+
+	std::cout << "test sequential naive multiply:" << std::endl;
+	double A[4] = { 2., 0., 0., 3. };
+	double B[4] = { 2., 0., 1., 2. };
+	double C[4];
+	sequential_naive_multiply(A, B, C, 2, 2, 2);
+	std::cout << "C: " << C[0] << " " << C[1] << " " << C[2] << " " << C[3] << std::endl;
 }
