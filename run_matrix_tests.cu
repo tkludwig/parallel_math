a8c308c0b4a84f86fe2fbceec8273ad1b942927e
tom ludwig <tom.k.ludwig@gmail.com>
2020-12-21 16:30:34 -0800

    compare openMP, sequential results

--- run_matrix_tests.cu
+++ run_matrix_tests.cu
@@ -18,34 +18,39 @@ int main()
 	std::cout << "Armadillo version: " << arma::arma_version::as_string() << std::endl;
 	const int M = 79;
 	const int N = 79;
-	double mat1[M*N];
-	double mat2[M*N];
-	double mat3[M*N];
-	int init_failed = random_uniform_initialize(mat1, M, N, 0.0, 0.5);
+	double mat1[M * N];
+	double mat2[M * N];
+	double mat3[M * N];
+	double mat4[M * N];
+	random_uniform_initialize(mat1, M, N, 0.0, 0.5);
+	random_uniform_initialize(mat2, M, N, 0.0, 0.5);
 
-	std::cout << "test sequential naive multiply:" << std::endl;
-	double A[4] = { 2., 0., 0., 3. };
-	double B[4] = { 2., 0., 1., 2. };
-	double C[4];
-	naive_multiply_sequential(A, B, C, 2, 2, 2);
-	print_matrix(C, 2, 2);
+	arma::Mat<double> a_mat1 = arma::mat(mat1, M, N, true);
+	arma::Mat<double> a_mat2 = arma::mat(mat2, M, N, true);
+	arma::Mat<double> a_mat3 = arma::mat(M, N);
 
 	std::cout << "test omp naive multiply:" << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
-	naive_multiply_omp(mat1, mat1, mat2, M, N, N);
+	naive_multiply_omp(mat1, mat2, mat3, M, N, N);
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 	std::cout << "naive_multiple_omp on size " << M << " took " << duration.count() << " ms" << std::endl;
 
 	start = std::chrono::high_resolution_clock::now();
-	naive_multiply_sequential(mat1, mat1, mat3, M, N, N);
+	naive_multiply_sequential(mat1, mat2, mat4, M, N, N);
 	end = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 	std::cout << "naive_multiple_sequential on size " << M << " took " << duration.count() << " ms" << std::endl;
 
-	double diff = max_diff(mat2, mat3, M, N);
-	std::cout << "diff between sequential and omp matrices: " << diff << std::endl;
+	//a_mat3 = a_mat1 * a_mat2; //the libraries required for this are not linking
 
-	print_matrix_big(mat2, M, N, 3);
+	double diff = max_diff(mat3, mat4, M, N);
+	std::cout << "diff between sequential and omp matrices: " << diff << std::endl;
+	diff = max_diff(mat3, a_mat3.memptr(), M, N);
+	std::cout << "diff between omp and armadillo results: " << diff << std::endl;
+
+
 	print_matrix_big(mat3, M, N, 3);
+	print_matrix_big(mat4, M, N, 3);
+	print_matrix_big(a_mat3.memptr(), M, N, 3);
 }
